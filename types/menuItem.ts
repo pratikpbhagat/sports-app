d3ff8e2ce84853631d6650172907bee11f73c052
pratikpbhagat <pratikpbhagat@gmail.com>
2025-09-30 16:30:14 +0530

    Delete types directory

--- types/menuItem.ts
+++
@@ -1,7 +0,0 @@
-export type MenuItem = {
-    title: string;
-    icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-    url: string;
-    component: React.ComponentType;
-    children?: MenuItem[];
-};
